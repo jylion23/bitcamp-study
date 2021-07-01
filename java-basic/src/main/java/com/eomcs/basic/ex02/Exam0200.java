3d2ae6e862f3cada96c7aecaff217a5f65a68e2f
jylion23 <jylion23@github.com>
2021-07-01 12:48:05 +0900

    .

---
+++ java-basic/src/main/java/com/eomcs/basic/ex02/Exam0200.java
@@ -0,0 +1,17 @@
+package com.eomcs.lang.ex02;
+
+/**
+클래스에 대한 설명
+ @author lee    
+*/
+public class Exam0200 {
+
+    public static String message = "Hello, World!";
+
+/**
+    @param args 앱 아규먼트 값을 보관한 배열
+ */
+    public static void main(String[] args) {
+        System.out.println(message);
+    }
+}
